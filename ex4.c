3d1f720c9d3e34817255b10ee102aa3cf4175dff
igarashi-s1280216 <63778539+igarashi-s1280216@users.noreply.github.com>
2021-07-08 22:29:38 +0900

    Initial revision

---
+++ ex4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int i,sum=0;
+   
+    printf("Roling the dice...\n");
+
+    srand((unsigned int)time(NULL));
+
+    for(int i=0;i<2;i++){
+        printf("Dice %d: ",i+1);
+
+        int num=rand()%6+1;
+        printf("%d\n",num);
+        sum+=num;
+    }
+    printf("Total value: %d\n",sum);
+    if(sum>=7) printf("%s won!",str);
+    else printf("%s lose!",str);
+
+    return 0;
+}
