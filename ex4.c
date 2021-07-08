cafaac2e18831c349dec9ba8f92a90a6a49a703a
igarashi-s1280216 <63778539+igarashi-s1280216@users.noreply.github.com>
2021-07-08 22:34:51 +0900

    Initial revision

--- ex4.c
+++ ex4.c
@@ -18,7 +18,7 @@ int main(){
     }
     printf("Total value: %d\n",sum);
     if(sum>=7) printf("%s won!",str);
-    else printf("%s lose!",str);
+    else printf("%s lose!",str)
 
     return 0;
 }
